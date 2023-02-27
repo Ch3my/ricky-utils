1f7ae2416f65303f603acf418dc5cb3f16a33943
Ch3my <joseconchan1@gmail.com>
2023-02-27 09:45:09 -0300

    Elimina colores manuales now use Printy

--- reporte-asistencia/reporte-asistencia.py
+++ reporte-asistencia/reporte-asistencia.py
@@ -17,18 +17,6 @@ import glob
 from printy import printy
 
 
-class TerminalColors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    ERR = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-
-
 class ExcelColors:
     yellow = "FFFF00"
     red = "FF0000"
